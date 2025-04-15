f29e48df4c9f396532a270ee87981c2897720198
root <root@sup-keycloak1.sup.itsx>
2025-04-15 14:10:18 +0500

    tee added to write to a file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+LOG=/var/log/app.log
 n=-1
 c=0
 if [ -n "$3" ]
@@ -13,13 +14,13 @@ do
    I=$(shuf -i 1-4 -n 1)
    D=`date -Iseconds`
    case "$I" in
-      "1") echo "$D ERROR An error is usually an exception that has been caught and not handled."
+      "1") echo "$D ERROR An error is usually an exception that has been caught and not handled." | tee -a $LOG
       ;;
-      "2") echo "$D INFO This is less important than debug log and is often used to provide context in the current task."
+      "2") echo "$D INFO This is less important than debug log and is often used to provide context in the current task." | tee -a $LOG
       ;;
-      "3") echo "$D WARN A warning that should be ignored is usually at this level and should be actionable."
+      "3") echo "$D WARN A warning that should be ignored is usually at this level and should be actionable." | tee -a $LOG
       ;;
-      "4") echo "$D DEBUG This is a debug log that shows a log that can be ignored."
+      "4") echo "$D DEBUG This is a debug log that shows a log that can be ignored." | tee -a $LOG
       ;;
    esac
    c=$(( c+1 ))
